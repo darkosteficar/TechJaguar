ff8ae9bb4d5369f5a6a5359023c6e14e2722de9d
darkosteficar <63226888+darkosteficar@users.noreply.github.com>
2021-04-12 11:30:58 +0200

    working on categories page

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -16,7 +16,41 @@
             <p class=" font-light text-xl text-gray-200 pl-5 py hover:bg-green-500 hover:text-gray-800 border-2 border-transparent hover:border-green-400">Graphics Cards</p>
         
         </div>
-        <div class="w-6/12">AMD News</div>
+        <div class="w-8/12 px-5 ">
+            <p class="mt-2 ml-2 font-bold text-2xl text-green-500 ">AMD</p>
+            <div class="flex  flex-wrap">
+                <div class=" w-1/3 p-2 ">
+                    <div class="relative ">
+                        <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72 border-2 border-gray-600 hover:border-green-500">
+                        <div class="absolute top-1 left-1">
+                            <p class="text-green-400 px-1 font-semibold bg-gray-600 border-2 border-transparent">PRESS RELEASE</p>
+                        </div>
+                        <div class="mx-1 relative ">
+                            <div class="absolute bottom-1  w-full pr-2  bg-opacity-40 bg-gray-400">
+                                <div class="opacity-100">
+                                    <p class="border-l-4 border-green-500">Date</p>
+                                    <p class="border-l-4 border-green-500">Title</p>
+                                </div>
+                               
+                            </div>
+                        </div>
+                        
+                    </div>
+                  
+                </div>
+                <div class="relative w-1/3">
+                    <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72">
+                </div>
+                <div class="relative w-1/3">
+                    <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72">
+                </div>
+                <div class="relative w-1/3">
+                    <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72">
+                </div>
+            </div>
+           
+        
+        </div>
         <div class="w-2/12 bg-gray-700 mr-2 rounded-sm">           
             <p class="font-normal text-2xl text-green-500 ml-2 my-2 text-center">POPULAR THIS WEEK</p>
             <div class="flex border border-transparent hover:border-white justify-between bg-gray-500 pt-1 hover:bg-green-500 text-green-400 hover:text-gray-800 mb-1">
