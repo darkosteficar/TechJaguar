2504143503265dbf835ba19d2b8746bcbef3b903
darkosteficar <63226888+darkosteficar@users.noreply.github.com>
2021-04-12 12:35:08 +0200

    done with news part for now,going to the 2nd part

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex justify-between mt-12 ">
-        <div class="w-2/12 bg-gray-700 ml-2 ">
+    <div class="flex justify-between my-12 ">
+        <div class="w-2/12 bg-gray-700 ml-2 border border-white rounded-sm">
             
-            <p class="font-normal text-3xl text-green-500 ml-2 my-2 ">BROWSE</p>
+            <p class="font-semibold text-xl text-green-500 ml-2 my-2 ">BROWSE</p>
             <p class="bg-green-500 font-light text-xl text-gray-800 pl-5 py border border-transparent hover:border-green-400">News</p>
             <p class=" font-light text-xl text-gray-200 pl-5 py hover:bg-green-500 hover:text-gray-800 border-2 border-transparent hover:border-green-400">Leaks</p>
             <p class=" font-light text-xl text-gray-200 pl-5 py hover:bg-green-500 hover:text-gray-800 border-2 border-transparent hover:border-green-400">Rumours</p>
@@ -17,19 +17,23 @@
         
         </div>
         <div class="w-8/12 px-5 ">
-            <p class="mt-2 ml-2 font-bold text-2xl text-green-500 ">AMD</p>
-            <div class="flex  flex-wrap">
+            <p class="my-2 ml-2 font-bold text-3xl text-green-500 ">AMD</p>
+            <div class="flex flex-wrap">
                 <div class=" w-1/3 p-2 ">
                     <div class="relative ">
-                        <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72 border-2 border-gray-600 hover:border-green-500">
+                        <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72 border-2 border-gray-600 hover:border-green-500 duration-300 ease-in">
                         <div class="absolute top-1 left-1">
                             <p class="text-green-400 px-1 font-semibold bg-gray-600 border-2 border-transparent">PRESS RELEASE</p>
                         </div>
                         <div class="mx-1 relative ">
                             <div class="absolute bottom-1  w-full pr-2  bg-opacity-40 bg-gray-400">
-                                <div class="opacity-100">
-                                    <p class="border-l-4 border-green-500">Date</p>
-                                    <p class="border-l-4 border-green-500">Title</p>
+                                <div class="opacity-100 ">
+                                    <div class="flex">
+                                        <img src="images/date-icon.png" alt="" width="25" class="border-l-4 border-green-500 object-scale-down mr-1 pl-2">
+                                        <p class=" font-semibold text-md text-green-500  ">12.1.2021 6:28</p>
+                                    </div>
+                                    
+                                    <p class="border-l-4 border-green-500 text-gray-100 text-xl font-semibold pl-2 pb-1">New AMD GPUs are coming in September</p>
                                 </div>
                                
                             </div>
@@ -38,22 +42,82 @@
                     </div>
                   
                 </div>
-                <div class="relative w-1/3">
-                    <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72">
+                <div class=" w-1/3 p-2 ">
+                    <div class="relative ">
+                        <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72 border-2 border-gray-600 hover:border-green-500 duration-300 ease-in">
+                        <div class="absolute top-1 left-1">
+                            <p class="text-green-400 px-1 font-semibold bg-gray-600 border-2 border-transparent">PRESS RELEASE</p>
+                        </div>
+                        <div class="mx-1 relative ">
+                            <div class="absolute bottom-1  w-full pr-2  bg-opacity-40 bg-gray-400">
+                                <div class="opacity-100 ">
+                                    <div class="flex">
+                                        <img src="images/date-icon.png" alt="" width="25" class="border-l-4 border-green-500 object-scale-down mr-1 pl-2">
+                                        <p class=" font-semibold text-md text-green-500  ">12.1.2021 6:28</p>
+                                    </div>
+                                    
+                                    <p class="border-l-4 border-green-500 text-gray-100 text-xl font-semibold pl-2 pb-1">New AMD GPUs are coming in September</p>
+                                </div>
+                               
+                            </div>
+                        </div>
+                        
+                    </div>
+                  
                 </div>
-                <div class="relative w-1/3">
-                    <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72">
+                <div class=" w-1/3 p-2 ">
+                    <div class="relative ">
+                        <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72 border-2 border-gray-600 hover:border-green-500 duration-300 ease-in">
+                        <div class="absolute top-1 left-1">
+                            <p class="text-green-400 px-1 font-semibold bg-gray-600 border-2 border-transparent">PRESS RELEASE</p>
+                        </div>
+                        <div class="mx-1 relative ">
+                            <div class="absolute bottom-1  w-full pr-2  bg-opacity-40 bg-gray-400">
+                                <div class="opacity-100 ">
+                                    <div class="flex">
+                                        <img src="images/date-icon.png" alt="" width="25" class="border-l-4 border-green-500 object-scale-down mr-1 pl-2">
+                                        <p class=" font-semibold text-md text-green-500  ">12.1.2021 6:28</p>
+                                    </div>
+                                    
+                                    <p class="border-l-4 border-green-500 text-gray-100 text-xl font-semibold pl-2 pb-1">New AMD GPUs are coming in September</p>
+                                </div>
+                               
+                            </div>
+                        </div>
+                        
+                    </div>
+                  
                 </div>
-                <div class="relative w-1/3">
-                    <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72">
+                <div class=" w-1/3 p-2 ">
+                    <div class="relative ">
+                        <img src="https://cdn.vox-cdn.com/thumbor/cwVnQuFa-mDsY5MCNygm0IEBCzI=/0x0:1260x709/1200x800/filters:focal(530x255:730x455)/cdn.vox-cdn.com/uploads/chorus_image/image/69092746/762922_amd_radeon_rx_6800xt_black_box_1260x709.0.jpg" alt="" class="object-cover h-72 border-2 border-gray-600 hover:border-green-500 duration-300 ease-in">
+                        <div class="absolute top-1 left-1">
+                            <p class="text-green-400 px-1 font-semibold bg-gray-600 border-2 border-transparent">PRESS RELEASE</p>
+                        </div>
+                        <div class="mx-1 relative ">
+                            <div class="absolute bottom-1  w-full pr-2  bg-opacity-40 bg-gray-400">
+                                <div class="opacity-100 ">
+                                    <div class="flex">
+                                        <img src="images/date-icon.png" alt="" width="25" class="border-l-4 border-green-500 object-scale-down mr-1 pl-2">
+                                        <p class=" font-semibold text-md text-green-500  ">12.1.2021 6:28</p>
+                                    </div>
+                                    
+                                    <p class="border-l-4 border-green-500 text-gray-100 text-xl font-semibold pl-2 pb-1">New AMD GPUs are coming in September</p>
+                                </div>
+                               
+                            </div>
+                        </div>
+                        
+                    </div>
+                  
                 </div>
             </div>
            
         
         </div>
-        <div class="w-2/12 bg-gray-700 mr-2 rounded-sm">           
-            <p class="font-normal text-2xl text-green-500 ml-2 my-2 text-center">POPULAR THIS WEEK</p>
-            <div class="flex border border-transparent hover:border-white justify-between bg-gray-500 pt-1 hover:bg-green-500 text-green-400 hover:text-gray-800 mb-1">
+        <div class="w-2/12 bg-gray-700 mr-2 rounded-sm border border-white">           
+            <p class="font-semibold text-xl text-green-500 ml-5 my-2 text-left">POPULAR THIS WEEK</p>
+            <div class="flex border border-transparent hover:border-white justify-between bg-gray-600 pt-1 hover:bg-green-500 text-green-400 hover:text-gray-800 mb-1">
                 <div class="w-1/2 ml-3">
                     <img src="https://static.gamespot.com/uploads/original/1568/15683559/3336534-graphics%20rx%2064.png" alt="" width="200" class="border border-transparent hover:border-green-300">    
                 </div>
@@ -66,7 +130,7 @@
                     
                 </div>
             </div>
-            <div class="flex border border-transparent hover:border-white justify-between bg-gray-500 pt-1 hover:bg-green-500 text-green-400 hover:text-gray-800">
+            <div class="flex border border-transparent hover:border-white justify-between bg-gray-600 pt-1 hover:bg-green-500 text-green-400 hover:text-gray-800">
                 <div class="w-1/2 ml-3">
                     <img src="https://static.gamespot.com/uploads/original/1568/15683559/3336534-graphics%20rx%2064.png" alt="" width="200" class="border border-transparent hover:border-green-300">    
                 </div>
